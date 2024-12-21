d9c6e28a12d2429e0f8ad06f099a02bb8ce582bc
Bhuvana-pro <bhuvabhu132004@gmail.com>
2024-12-21 17:21:33 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_xdaEVfWz9', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ruiiu4j5nkerninlsu7ieoe4j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_V1moWeZ1c', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2uiismtre93mui59fa1nmc54lu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
